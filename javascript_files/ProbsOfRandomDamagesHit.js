e518306a8873049ee24968c550bdc49afe718b30
giuoaejgiusejnb <takuya19991227@gmail.com>
2026-01-17 07:57:20 +0900

    Update ProbsOfRandomDamagesHit.js

--- javascript_files/ProbsOfRandomDamagesHit.js
+++ javascript_files/ProbsOfRandomDamagesHit.js
@@ -1,4 +1,3 @@
-alert("hello");
 import { repetitionCombination } from "./repetitionCombination.js";
 
 // フォロワーやリーダーにランダム打点を飛ばすとき，指定したフォロワーやリーダーに指定した回数打点が飛ぶ確率を求めるクラス
@@ -99,6 +98,7 @@ class ProbsOfRandomDamagesHit {
 
 
 export {ProbsOfRandomDamagesHit};
+
 
 
 
